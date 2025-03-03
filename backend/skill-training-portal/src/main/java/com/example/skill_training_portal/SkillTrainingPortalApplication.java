19139fafa8002756bd145b4fb83f30b86f18248b
Shadow310524 <harishrenga2005@gmail.com>
2025-03-03 20:42:43 +0530

    Moved frontend files into a separate folder

---
+++ backend/skill-training-portal/src/main/java/com/example/skill_training_portal/SkillTrainingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.skill_training_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTrainingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTrainingPortalApplication.class, args);
+	}
+
+}
---
+++ backend/skill-training-portal/src/test/java/com/example/skill_training_portal/SkillTrainingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skill_training_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTrainingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
